2a65a58839e49e12053b992f4e7c843a4b2e10c4
Gregory ARTHUS <garthus@adminux.fr>
2021-02-15 10:12:44 +0100

    task 6

--- console.py
+++ console.py
@@ -4,11 +4,21 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 
-    def do_greet(self, line):
-        print("hello")
+    def emptyline(self):
+        pass
+
+    def do_create(self, line):
+
 
     def do_EOF(self, line):
+        """Quit shortcut to exit the program\n"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
         return True
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    prompt = HBNBCommand()
+    prompt.prompt = '(hbnb) '
+    prompt.cmdloop()
