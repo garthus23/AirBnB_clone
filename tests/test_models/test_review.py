8541d14c56f00805570d992c01d46c9ec0c97124
Gregory ARTHUS <garthus@adminux.fr>
2021-02-17 09:39:12 +0000

    all tests files

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,32 @@
+import unittest
+from models.base_model import BaseModel
+
+class TestClass(unittest.TestCase):
+
+
+    def test_one_instance(self):
+        b1 = BaseModel()
+        b1.name = "Holberton"
+        self.assertTrue(b1.name)
+
+    def test_an_other_value(self):
+        b1 = BaseModel()
+        b1.my_number = 23
+        self.assertTrue(b1.my_number)
+
+    def test_verify_id(self):
+        b1 = BaseModel()
+        self.assertTrue(b1.id)
+
+    def test_verify_create_date(self):
+        b1 = BaseModel()
+        self.assertTrue(b1.created_at)
+
+    def test_verify_updated_date(self):
+        b1 = BaseModel()
+        self.assertTrue(b1.updated_at)
+
+    def test_verify_class_exist(self):
+        b1 = BaseModel()
+        self.assertTrue(b1.__class__)
+
